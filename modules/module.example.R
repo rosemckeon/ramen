d00809511978764717c31049c1ee1cbce6f96fc0
Rose <rosemckeon@gmail.com>
2017-05-28 23:51:34 +0100

    template module created

---
+++ modules/module.example.R
@@ -0,0 +1,18 @@
+# copy to your_module.R and keep this template empty for future
+library(shiny)
+
+# UI portion of your_module
+# @param id can be set when the module is loaded in ui.R
+your_moduleUI <- function(id) {
+  # set the id as name-space for ui elements
+  ns <- NS(id)
+  # put your ui elements for inputs and output here
+  # See: ?ns for usage examples
+}
+
+# server portion of your_module
+your_module <- function(input, output, session) {
+  # get the session name-space set by ui id
+  ns = session$ns
+  # put your server functions here
+}
